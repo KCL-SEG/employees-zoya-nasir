22d82e05234b32f83d2a162c3330fdadf067a848
Zoya Nasir <k22031112@kcl.ac.uk>
2023-09-28 18:12:09 +0100

    Completed exercise

--- employee.py
+++ employee.py
@@ -2,30 +2,88 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 class Employee:
-    def __init__(self, name):
+    
+    def __init__(self, name, contract_type, wage_or_salary, commission_type = None, commission_rate = None, hours_worked = None, contracts_landed = None):
         self.name = name
+        self.contract_type = contract_type
+        self.wage_or_salary = wage_or_salary
+        self.commission_type = commission_type
+        self.commission_rate = commission_rate
+        self.hours_worked = hours_worked
+        self.contracts_landed = contracts_landed
+        self.commission_pay = 0
+        self.commission_exp = " "
 
     def get_pay(self):
-        pass
+        
+        if self.contract_type == "Salary":
+            contract_pay = self.wage_or_salary
+            
+        elif self.contract_type == "Hourly":
+            if self.hours_worked is None:
+                raise ValueError("The number of hours worked must be provided for hourly employees.")
+            
+            contract_pay = self.wage_or_salary * self.hours_worked
+            
+        else:
+            raise ValueError("Invalid contract type!")
+
+        commission_pay = 0
+        commission_exp = " "
+
+        if self.commission_type == "Bonus":
+            if self.commission_rate is None:
+                raise ValueError("The commission rate must be provided for employees with a bonus commission.")
+            
+            commission_pay = self.commission_rate
+            commission_exp = (f" and receives a bonus commission of {self.commission_rate}.")
+            
+        elif self.commission_type == "Contract":
+            if self.commission_rate is None or self.contracts_landed is None:
+                raise ValueError("Commission rate and number of contracts landed must be provided for employees with a contract commission.")
+            
+            commission_pay = self.commission_rate * self.contracts_landed
+            commission_exp = (f" and receives a commission for {self.contracts_landed} contract(s) at {self.commission_rate}/contract.")
+
+        total_pay = contract_pay + commission_pay
+        return total_pay
 
     def __str__(self):
-        return self.name
+        
+        contract_exp = " "
 
+        if self.contract_type == "Salary":
+            contract_exp = (f"works on a monthly salary of {self.wage_or_salary}")
 
-# Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie')
+        elif self.contract_type == "Hourly":
+            if self.hours_worked is None:
+                raise ValueError("Hours worked must be provided for hourly employees.")
+            contract_exp = (f"works on a contract of {self.hours_worked} hours at {self.wage_or_salary}/hour.")
 
-# Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie')
+        pay_exp = (f"Their total pay is {self.get_pay()}.")
+        return (f"{self.name} {contract_exp}{self.commission_exp} {pay_exp}")
 
-# Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee')
+billie = Employee('Billie', 'Salary', 4000)
+charlie = Employee('Charlie', 'Hourly', 25, hours_worked = 100)
+renee = Employee('Renee', 'Salary', 3000, 'Contract', 200, contracts_landed = 4)
+jan = Employee('Jan', 'Hourly', 25, 'Contract', 220, hours_worked=150, contracts_landed = 3)
+robbie = Employee('Robbie', 'Salary', 2000, 'Bonus', 1500)
+ariel = Employee('Ariel', 'Hourly', 30, 'Bonus', 600, hours_worked = 120)
 
-# Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan')
+print(billie.get_pay())  
+print(str(billie))  
 
-# Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie')
+print(charlie.get_pay())  
+print(str(charlie)) 
 
-# Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel')
+print(renee.get_pay())  
+print(str(renee))
+
+print(jan.get_pay())  
+print(str(jan))  
+
+print(robbie.get_pay()) 
+print(str(robbie))  
+
+print(ariel.get_pay())  
+print(str(ariel))  
