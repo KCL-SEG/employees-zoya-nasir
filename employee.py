e286b2f48d8bee9ded20ddae399739fa28973065
Zoya Nasir <k22031112@kcl.ac.uk>
2023-09-28 18:28:28 +0100

    Final Completion

--- employee.py
+++ employee.py
@@ -35,17 +35,17 @@ class Employee:
             if self.commission_rate is None:
                 raise ValueError("The commission rate must be provided for employees with a bonus commission.")
             
-            commission_pay = self.commission_rate
-            commission_exp = (f" and receives a bonus commission of {self.commission_rate}.")
+            self.commission_pay = self.commission_rate
+            self.commission_exp = (f" and receives a bonus commission of {self.commission_rate},")
             
         elif self.commission_type == "Contract":
             if self.commission_rate is None or self.contracts_landed is None:
                 raise ValueError("Commission rate and number of contracts landed must be provided for employees with a contract commission.")
             
-            commission_pay = self.commission_rate * self.contracts_landed
-            commission_exp = (f" and receives a commission for {self.contracts_landed} contract(s) at {self.commission_rate}/contract.")
+            self.commission_pay = self.commission_rate * self.contracts_landed
+            self.commission_exp = (f" and receives a commission for {self.contracts_landed} contract(s) at {self.commission_rate}/contract,")
 
-        total_pay = contract_pay + commission_pay
+        total_pay = contract_pay + self.commission_pay
         return total_pay
 
     def __str__(self):
@@ -53,20 +53,21 @@ class Employee:
         contract_exp = " "
 
         if self.contract_type == "Salary":
-            contract_exp = (f"works on a monthly salary of {self.wage_or_salary}")
+            contract_exp = (f"works on a monthly salary of {self.wage_or_salary},")
 
         elif self.contract_type == "Hourly":
             if self.hours_worked is None:
                 raise ValueError("Hours worked must be provided for hourly employees.")
-            contract_exp = (f"works on a contract of {self.hours_worked} hours at {self.wage_or_salary}/hour.")
+            
+            contract_exp = (f"works on a contract of {self.hours_worked} hours at {self.wage_or_salary}/hour,")
 
-        pay_exp = (f"Their total pay is {self.get_pay()}.")
+        pay_exp = (f"their total pay is {self.get_pay()}.")
         return (f"{self.name} {contract_exp}{self.commission_exp} {pay_exp}")
 
 billie = Employee('Billie', 'Salary', 4000)
 charlie = Employee('Charlie', 'Hourly', 25, hours_worked = 100)
 renee = Employee('Renee', 'Salary', 3000, 'Contract', 200, contracts_landed = 4)
-jan = Employee('Jan', 'Hourly', 25, 'Contract', 220, hours_worked=150, contracts_landed = 3)
+jan = Employee('Jan', 'Hourly', 25, 'Contract', 220, hours_worked = 150, contracts_landed = 3)
 robbie = Employee('Robbie', 'Salary', 2000, 'Bonus', 1500)
 ariel = Employee('Ariel', 'Hourly', 30, 'Bonus', 600, hours_worked = 120)
 
